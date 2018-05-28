e0232f261070a290f7ca76a89639cc1f218deb84
Karla Brkić <karla.brkic@gmail.com>
2018-05-28 17:15:31 +0200

    Stats on random and delayed trading.

--- backtesting/config.py
+++ backtesting/config.py
@@ -2,7 +2,6 @@ database_config = {
   'user': 'root',
   'password': 'karla',
   'host': '127.0.0.1',
-#  'database': 'prodclone_core_2018_04_23',
-  'database': 'prodclone_core_2018_05_18', # trend for RSI switched
+  'database': 'prodclone_core_2018_05_26',
   'raise_on_warnings': True,
 }
