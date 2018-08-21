1b17b62b73874d9b24e7fd1d4f32d4f82a8d52c9
sodogemuchwow <doge1337wow@gmail.com>
2018-08-21 15:38:54 +0200

    Ugly, but finally able to add different grammar variants to Artemis experiments.

--- genetic_algorithms/gp_artemis.py
+++ genetic_algorithms/gp_artemis.py
@@ -1,8 +1,10 @@
 from artemis.experiments import experiment_function
 from gp_data import Data
 from data_sources import Horizon
-from genetic_program import GeneticProgram
+from genetic_program import GeneticProgram, FitnessFunctionV1
+from leaf_functions import TAProvider
 import logging
+from grammar import GrammarV2, GrammarV1
 
 @experiment_function
 def run_evolution(genetic_program, mating_prob, mutation_prob, population_size, num_generations):
@@ -10,7 +12,7 @@ def run_evolution(genetic_program, mating_prob, mutation_prob, population_size, 
     return hof, best
 
 
-def build_gp():
+def construct_data():
     transaction_currency = "OMG"
     counter_currency = "BTC"
     end_time = 1526637600
@@ -31,28 +33,51 @@ def build_gp():
                            resample_period, horizon, start_cash, start_crypto, source)
     """
 
-    data = training_data
-    return GeneticProgram(data)
+    return training_data
+
 
 
 def register_variants():
-    # get gprog
-    gprog = build_gp()
+    data = construct_data()
+
+    function_provider = TAProvider(data=data)
+
+    gprog = GeneticProgram(data, function_provider=function_provider, grammar=GrammarV1(function_provider=function_provider))
+
     # add variants of experiments
     variant1 = run_evolution.add_variant(variant_name='evolution_v1', genetic_program=gprog, mating_prob=0.7, mutation_prob=0.5,
                                          population_size=50, num_generations=2)
+    variant1.run()
+
+
     variant2 = run_evolution.add_variant(variant_name='evolution_v2', genetic_program=gprog, mating_prob=0.5, mutation_prob=0.7,
                                          population_size=50,
                                          num_generations=3)
-    return variant1, variant2
+    variant2.run()
+
+    #return variant1, variant1, variant1
+
+
+    gprog2 = GeneticProgram(data, function_provider=function_provider, grammar=GrammarV2(function_provider=function_provider))
+    #gprog2 = GeneticProgram(data, fitness_function=FitnessFunctionV1())
+
+    variant3 = \
+        run_evolution.add_variant(variant_name='evolution_v3', genetic_program=gprog2, mating_prob=0.5, mutation_prob=0.7,
+                                         population_size=50,
+                                         num_generations=3)
+    variant3.run()
+
+    return variant1, variant2, variant3
 
 def run_experiments():
     variant1.run(keep_record=True, display_results=True)
     variant2.run(keep_record=True, display_results=True)
+    #variant3.run(keep_record=True, display_results=True)
+
 
 
 def explore_records():
-    variant_names = ['evolution_v1', 'evolution_v2']
+    variant_names = ['evolution_v1', 'evolution_v2', 'evolution_v3']
     # variants = run_evolution.get_all_variants(include_roots=True, include_self=True)
     for variant_name in variant_names:
         variant = run_evolution.get_variant(variant_name)
@@ -78,8 +103,8 @@ def explore_records():
 ####################################
 
 if __name__ == "__main__":
-    variant1, variant2 = register_variants()
-#    run_experiments()
-    explore_records()
+    variant1, variant2, variant3 = register_variants()
+    #run_experiments()
+    #explore_records()
 
 
--- genetic_algorithms/grammar.py
+++ genetic_algorithms/grammar.py
@@ -1,8 +1,8 @@
 import operator
 import random
 import types
-
 from deap import gp
+
 from abc import ABC, abstractmethod
 
 
@@ -12,9 +12,8 @@ class Grammar(ABC):
         self.function_provider = function_provider
 
     @property
-    @abstractmethod
     def pset(self):
-        pass
+        return self._pset
 
     @property
     @abstractmethod
@@ -24,15 +23,22 @@ class Grammar(ABC):
 
 class GrammarV1(Grammar):
 
-    def __init__(self, function_provider):
+    def __init__(self, function_provider, ephemeral_prefix=""):
         super(GrammarV1, self).__init__(function_provider)
+        self._ephemeral_prefix = ephemeral_prefix  # we need this because Deap stores the grammar in global namespace
+                                                   # this creates issues if re-running evolution with unchanged
+                                                   # ephemeral constant names!
+                                                   # see https://github.com/DEAP/deap/issues/108
         self._build_grammar()
 
     def name(self):
         return("g_v1")
 
     def _build_grammar(self):
-        pset = gp.PrimitiveSetTyped("main", [list], types.FunctionType)
+        #import importlib, deap
+        #gp = importlib.reload(deap.gp)
+
+        pset = gp.PrimitiveSetTyped(f"main-{self.name}", [list], types.FunctionType)
         pset.addPrimitive(operator.lt, [float, float], bool)
         pset.addPrimitive(operator.gt, [float, float], bool)
         pset.addPrimitive(operator.or_, [bool, bool], bool)
@@ -52,10 +58,49 @@ class GrammarV1(Grammar):
         pset.addPrimitive(self.function_provider.identity, [bool], bool, name="identity_bool")
         pset.addPrimitive(self.function_provider.identity, [list], list, name="identity_list")
         pset.addPrimitive(self.function_provider.identity, [float], float, name="identity_float")
-        pset.addEphemeralConstant("rsi_overbought_threshold", lambda: random.uniform(70, 100), float)
-        pset.addEphemeralConstant("rsi_oversold_threshold", lambda: random.uniform(0, 30), float)
+        pset.addEphemeralConstant(f"rsi_overbought_threshold_{random.randint(0,1000)}", lambda: random.uniform(70, 100), float)
+        pset.addEphemeralConstant(f"rsi_oversold_threshold_{random.randint(0,1000)}", lambda: random.uniform(0, 30), float)
         self._pset = pset
 
-    @property
-    def pset(self):
-        return self._pset
+
+
+class GrammarV2(Grammar):
+
+    def __init__(self, function_provider, ephemeral_prefix=""):
+        super(GrammarV2, self).__init__(function_provider)
+        self._ephemeral_prefix = ephemeral_prefix  # we need this because Deap stores the grammar in global namespace
+                                                   # this creates issues if re-running evolution with unchanged
+                                                   # ephemeral constant names!
+                                                   # see https://github.com/DEAP/deap/issues/108
+        self._build_grammar()
+
+    def name(self):
+        return("g_v2")
+
+    def _build_grammar(self):
+        #import importlib, deap
+        #gp = importlib.reload(deap.gp)
+
+        pset = gp.PrimitiveSetTyped(f"main-{self.name}", [list], types.FunctionType)
+        pset.addPrimitive(operator.lt, [float, float], bool)
+        pset.addPrimitive(operator.gt, [float, float], bool)
+        pset.addPrimitive(operator.or_, [bool, bool], bool)
+        pset.addPrimitive(operator.and_, [bool, bool], bool)
+        pset.addPrimitive(self.function_provider.if_then_else, [bool, types.FunctionType, types.FunctionType], types.FunctionType)
+        pset.addPrimitive(self.function_provider.rsi, [list], float)
+        pset.addPrimitive(self.function_provider.sma50, [list], float)
+        pset.addPrimitive(self.function_provider.ema50, [list], float)
+        pset.addPrimitive(self.function_provider.sma200, [list], float)
+        pset.addPrimitive(self.function_provider.ema200, [list], float)
+        pset.addPrimitive(self.function_provider.price, [list], float)
+        pset.addTerminal(False, bool)
+        pset.addTerminal(True, bool)
+        pset.addTerminal(self.function_provider.buy, types.FunctionType)
+        pset.addTerminal(self.function_provider.sell, types.FunctionType)
+        pset.addTerminal(self.function_provider.ignore, types.FunctionType)
+        pset.addPrimitive(self.function_provider.identity, [bool], bool, name="identity_bool")
+        pset.addPrimitive(self.function_provider.identity, [list], list, name="identity_list")
+        pset.addPrimitive(self.function_provider.identity, [float], float, name="identity_float")
+        pset.addEphemeralConstant(f"rsi_overbought_threshold_{random.randint(0,1000)}", lambda: random.uniform(70, 100), float)
+        pset.addEphemeralConstant(f"rsi_oversold_threshold_{random.randint(0,1000)}", lambda: random.uniform(0, 30), float)
+        self._pset = pset
