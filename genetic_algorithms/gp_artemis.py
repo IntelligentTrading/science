7c7b6e17cf874a468f7a6456685d4de2c338c45a
sodogemuchwow <doge1337wow@gmail.com>
2018-08-22 13:55:09 +0200

    Added ExperimentDB class, cleanup.

--- genetic_algorithms/gp_artemis.py
+++ genetic_algorithms/gp_artemis.py
@@ -8,13 +8,15 @@ from grammar import GrammarV2, GrammarV1, Grammar
 
 
 @experiment_function
-def run_evolution(evolution_name, data, function_provider, grammar_version, fitness_function, mating_prob,
+def run_evolution(experiment_id, data, function_provider, grammar_version, fitness_function, mating_prob,
                   mutation_prob, population_size, num_generations):
-    grammar = Grammar.construct(grammar_version, function_provider, ephemeral_suffix=evolution_name)
+    grammar = Grammar.construct(grammar_version, function_provider, ephemeral_suffix=experiment_id)
     genetic_program = GeneticProgram(data=data, function_provider=function_provider,
                             grammar=grammar, fitness_function=fitness_function)
     hof, best = genetic_program.evolve(mating_prob, mutation_prob, population_size, num_generations)
     return hof, best
+
+
 
 def construct_data():
     transaction_currency = "OMG"
@@ -40,50 +42,94 @@ def construct_data():
     return training_data
 
 
-def register_variants(rebuild_grammar=False):
+class ExperimentDB:
+
+    def __init__(self):
+        self._experiments = {}
+        self._num_records = 0
+
+    def add(self, data, function_provider, grammar_version, fitness_function,
+            mating_prob, mutation_prob, population_size, num_generations):
+        entry = {}
+        entry['data'] = data
+        entry['function_provider'] = function_provider
+        entry['grammar_version'] = grammar_version
+        entry['fitness_function'] = fitness_function
+        entry['mating_prob'] = mating_prob
+        entry['mutation_prob'] = mutation_prob
+        entry['population_size'] = population_size
+        entry['num_generations'] = num_generations
+        entry['experiment_id'] = self._num_records
+        name = self.build_experiment_id(**entry)
+        self._experiments[name] = entry
+        self._num_records += 1
+
+    def get_all(self):
+        for k, v in self._experiments.items():
+            yield k, v
+
+    def build_experiment_id(**kwargs):
+        return f"data_{kwargs['data']};" \
+               f"provider_{kwargs['function_provider']};" \
+               f"grammar_{kwargs['grammar_version']};" \
+               f"fitness_{kwargs['fitness_function']};" \
+               f"matingprob_{kwargs['mating_prob']};" \
+               f"mutationprob_{kwargs['mutation_prob']};" \
+               f"populationsize_{kwargs['population_size']};" \
+               f"generations_{kwargs['num_generations']}"
+
+    # def build_experiment_id(**kwargs):
+    #    return ';'.join(['{}_{}'.format(k, v) for k, v in kwargs.iteritems()])
+
+
+def register_experiments():
+    experiments = ExperimentDB()
     data = construct_data()
     function_provider = TAProvider(data=data)
+
+    experiments.add(
+        data=data,
+        function_provider=function_provider,
+        grammar_version="gv1",
+        fitness_function=FitnessFunctionV1(),
+        mating_prob=0.7,
+        mutation_prob=0.5,
+        population_size=50,
+        num_generations=2)
+
+
+    experiments.add(  # variant_name='evolution_v2',
+        data=data,
+        function_provider=function_provider,
+        grammar_version="gv2",
+        fitness_function=FitnessFunctionV1(),
+        mating_prob=0.7,
+        mutation_prob=0.5,
+        population_size=50,
+        num_generations=2)
+
+    experiments.add(  # variant_name='evolution_v3',
+        data=data,
+        function_provider=function_provider,
+        grammar_version="gv1",
+        fitness_function=FitnessFunctionV2(),
+        mating_prob=0.7,
+        mutation_prob=0.5,
+        population_size=50,
+        num_generations=2)
+
+
+    return experiments
+
+def register_variants(rebuild_grammar=False):
+    experiments = register_experiments()
     if rebuild_grammar:
-        GrammarV1(function_provider=function_provider, ephemeral_suffix="ev1")
-        GrammarV1(function_provider=function_provider, ephemeral_suffix="ev2")
-        GrammarV1(function_provider=function_provider, ephemeral_suffix="ev3")
+        for experiment_name, experiment in experiments.get_all():
+            Grammar.construct(experiment['grammar_version'], experiment['function_provider'], experiment['experiment_id'])
 
-
-    variant1 = run_evolution.add_variant(#variant_name='evolution_v1',
-                                         evolution_name='ev1',
-                                         data=data,
-                                         function_provider=function_provider,
-                                         grammar_version="gv1",
-                                         fitness_function=FitnessFunctionV1(),
-                                         mating_prob=0.7,
-                                         mutation_prob=0.5,
-                                         population_size=50,
-                                         num_generations=2)
-
-    variant2 = run_evolution.add_variant(#variant_name='evolution_v2',
-                                         evolution_name='ev2',
-                                         data=data,
-                                         function_provider=function_provider,
-                                         grammar_version="gv2",
-                                         fitness_function=FitnessFunctionV1(),
-                                         mating_prob=0.7,
-                                         mutation_prob=0.5,
-                                         population_size=50,
-                                         num_generations=2)
-
-    variant3 = run_evolution.add_variant(#variant_name='evolution_v3',
-                                         data=data,
-                                         evolution_name='ev3',
-                                         function_provider=function_provider,
-                                         grammar_version="gv1",
-                                         fitness_function=FitnessFunctionV2(),
-                                         mating_prob=0.7,
-                                         mutation_prob=0.5,
-                                         population_size=50,
-                                         num_generations=2)
-
+    for experiment_name, experiment in experiments.get_all():
+        run_evolution.add_variant(variant_name=experiment_name, **experiment)
     return run_evolution.get_variants()
-
 
 
 def run_experiments(variants):
@@ -123,8 +169,8 @@ def explore_records(variants):
 ####################################
 
 if __name__ == "__main__":
-    variants = register_variants(rebuild_grammar=False)
-    run_experiments(variants)
+    variants = register_variants(rebuild_grammar=True)
+    #run_experiments(variants)
     explore_records(variants)
 
 
