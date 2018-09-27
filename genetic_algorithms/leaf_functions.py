b7b911c503667903928c4bfe372ae2b633ce415f
sodogemuchwow <doge1337wow@gmail.com>
2018-09-27 16:19:39 +0200

    TAProviderCollection dynamically creates functions based on TAProvider.

--- genetic_algorithms/leaf_functions.py
+++ genetic_algorithms/leaf_functions.py
@@ -1,5 +1,7 @@
 import numpy as np
+import inspect
 from collections import namedtuple
+
 
 class FunctionProvider:
 
@@ -24,7 +26,9 @@ class FunctionProvider:
     def identity(self, x):
         return x
 
+
 DataKey = namedtuple('DataKey', 'transaction_currency counter_currency start_time end_time')
+
 
 class TAProvider(FunctionProvider):
 
@@ -39,49 +43,56 @@ class TAProvider(FunctionProvider):
         timestamp_index = np.where(self.data.price_data.index == timestamp)[0]
         return self.data.rsi_data[timestamp_index]
 
-
     def sma50(self, input):
         timestamp = input[0]
         timestamp_index = np.where(self.data.price_data.index == timestamp)[0]
         return self.data.sma50_data[timestamp_index]
-
 
     def ema50(self, input):
         timestamp = input[0]
         timestamp_index = np.where(self.data.price_data.index == timestamp)[0]
         return self.data.ema50_data[timestamp_index]
 
-
     def sma200(self, input):
         timestamp = input[0]
         timestamp_index = np.where(self.data.price_data.index == timestamp)[0]
         return self.data.sma200_data[timestamp_index]
-
 
     def ema200(self, input):
         timestamp = input[0]
         timestamp_index = np.where(self.data.price_data.index == timestamp)[0]
         return self.data.ema200_data[timestamp_index]
 
-
     def price(self, input):
         timestamp = input[0]
-        try:
-            return self.data.price_data.loc[timestamp,"close_price"]
-        except:
-            print('OMG!!!')
+        return self.data.price_data.loc[timestamp,"close_price"]
 
 
-class TAProviderCollection(TAProvider):
-    # TODO: figure out how to write this shorter and DRY
+class TAProviderCollection(FunctionProvider):
 
     def __init__(self, data_collection):
         self.providers = {DataKey(data.transaction_currency, data.counter_currency,
-                                  data.start_time, data.end_time) : TAProvider(data) for data in data_collection}
+                                  data.start_time, data.end_time): TAProvider(data) for data in data_collection}
+        # create methods
+        members = inspect.getmembers(TAProvider, predicate=inspect.isfunction)
+        base_members = inspect.getmembers(FunctionProvider)
+        for member in members:
+            if member in base_members:
+                continue
+
+            function_name = member[0]
+            if function_name.startswith('__'):
+                continue
+
+            def fn(self, input):
+                timestamp, transaction_currency, counter_currency = input
+                return self.get_provider(timestamp, transaction_currency, counter_currency)[function_name]([timestamp])
+            fn.__name__ = function_name
+
+            setattr(TAProviderCollection, function_name, fn)
 
     def __str__(self):
         return("TAproviderCollection")
-
 
     def get_provider(self, timestamp, transaction_currency, counter_currency):
         for key in self.providers.keys():
@@ -91,32 +102,3 @@ class TAProviderCollection(TAProvider):
                 return self.providers[key]
         raise Exception(f'No data loaded for {transaction_currency} {counter_currency} at {timestamp}!')
 
-
-    def rsi(self, input):
-        timestamp, transaction_currency, counter_currency = input
-        return self.get_provider(timestamp, transaction_currency, counter_currency).rsi([timestamp])
-
-
-    def sma50(self, input):
-        timestamp, transaction_currency, counter_currency = input
-        return self.get_provider(timestamp, transaction_currency, counter_currency).sma50([timestamp])
-
-
-    def ema50(self, input):
-        timestamp, transaction_currency, counter_currency = input
-        return self.get_provider(timestamp, transaction_currency, counter_currency).ema50([timestamp])
-
-
-    def sma200(self, input):
-        timestamp, transaction_currency, counter_currency = input
-        return self.get_provider(timestamp, transaction_currency, counter_currency).sma200([timestamp])
-
-
-    def ema200(self, input):
-        timestamp, transaction_currency, counter_currency = input
-        return self.get_provider(timestamp, transaction_currency, counter_currency).ema200([timestamp])
-
-
-    def price(self, input):
-        timestamp, transaction_currency, counter_currency = input
-        return self.get_provider(timestamp, transaction_currency, counter_currency).price([timestamp])
